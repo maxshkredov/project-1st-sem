966bb7b77d956e9cde641bd8b310afd68f534d7f
maxshkredov <43685829+maxshkredov@users.noreply.github.com>
2018-12-15 21:04:05 +0300

    Add files via upload

--- mainfile.py
+++ mainfile.py
@@ -3,46 +3,124 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QDesktopWidget, \
      QMessageBox, QAction, QFileDialog, QLabel, QMenu, \
      QGraphicsView, QVBoxLayout, QWidget, QGraphicsScene, QGraphicsPathItem, \
      QColorDialog     
-from PyQt5.QtGui import QPixmap, QColor, QPen, QPainterPath, \
+from PyQt5.QtGui import QPixmap, QPen, QPainterPath, \
      QMouseEvent, QIcon
 from PyQt5.QtCore import Qt
 
+        
+class Pathitem(QGraphicsPathItem):
+    def __init__(self):
+        super(Pathitem, self).__init__()
 
+
+class Path(QPainterPath):
+    def __init__(self):
+        super(Path, self).__init__()
+
+        
 class Canvas(QGraphicsView):
     def __init__(self):
         super(Canvas, self).__init__()
+
+        global scene
+        scene = QGraphicsScene()
+        self.setScene(scene)
+        self.path1 = QPainterPath()
+        self.path2 = QPainterPath()
+
+        global new_path
+        new_path = Path()
+
+        global new_pathitem, pathitem1, pathitem
+        pathitem1 = QGraphicsPathItem()
+        pathitem = Pathitem()
+        new_pathitem = Pathitem()
         
-        self.setScene(QGraphicsScene())
-        self.path = QPainterPath()
-        self.item = Instrument()
-        self.scene().addItem(self.item)
+        global pen
+        pen = QPen(Qt.black, 10)
+        pathitem.setPen(pen)
+        
+        eraser = QPen(Qt.white, 10)
+        pathitem1.setPen(eraser)
+
+        global item
+        item = pathitem
+        scene.addItem(item)
 
     def mousePressEvent(self, event):
         self.start = self.mapToScene(event.pos())
-        self.path.moveTo(self.start)
+        global item, new_pathitem, new_path, pen
+        if item == pathitem1:
+            self.path1.moveTo(self.start)
+        elif item == new_pathitem:
+            new_path.moveTo(self.start)
+        else:
+            self.path2.moveTo(self.start)
 
     def mouseMoveEvent(self, event):
         self.end = self.mapToScene(event.pos())
-        self.path.lineTo(self.end)
-        self.start = self.end
-        self.item.setPath(self.path)
+        global item, new_pathitem, new_path
+        if item == pathitem:
+            self.path1.lineTo(self.end)
+            item.setPath(self.path1)
+        elif item == new_pathitem:
+            new_path.lineTo(self.end)
+            new_pathitem.setPath(new_path)
+        else:
+            self.path2.lineTo(self.end)
+            pathitem1.setPath(self.path2)
 
+    @classmethod   
+    def erase(self):
+        global item
+        item = pathitem1
+        scene.addItem(item)
 
-class Instrument(QGraphicsPathItem):
-    def __init__(self):
-        super(Instrument, self).__init__()
+    @classmethod  
+    def draw(self):
+        global item
+        item = pathitem
+        scene.addItem(item)
+    
+    @classmethod
+    def big_size(self):
+        global item, new_pathitem, new_path
+        pen.setWidth(15)
+        new_pathitem = Pathitem()
+        new_pathitem.setPen(pen)
+        new_path = Path()
+        item = new_pathitem
+        scene.addItem(item)
 
-        global pen
-        pen = QPen(Qt.black, 10)
-        self.setPen(pen)
+    @classmethod
+    def medium_size(self):
+        global item, new_pathitem, new_path
+        pen.setWidth(10)
+        new_pathitem = Pathitem()
+        new_pathitem.setPen(pen)
+        new_path = Path()
+        item = new_pathitem
+        scene.addItem(item)
+
+    @classmethod
+    def small_size(self):
+        global item, new_pathitem, new_path
+        pen.setWidth(5)
+        new_pathitem = Pathitem()
+        new_pathitem.setPen(pen)
+        new_path = Path()
+        item = new_pathitem
+        scene.addItem(item)
 
     @classmethod
     def colour(self):
+        global item, new_pathitem, new_path
         pen.setColor(QColorDialog.getColor())
-
-    @classmethod
-    def size(self):
-        pen.setWidth(15)
+        new_pathitem = Pathitem()
+        new_pathitem.setPen(pen)
+        new_path = Path()
+        item = new_pathitem
+        scene.addItem(item)
 
 
 class Window(QMainWindow):
@@ -58,40 +136,38 @@ class Window(QMainWindow):
         self.initUI()
 
     def initUI(self):
-        extractAction1 = QAction('Draw', self)
-        extractAction1.triggered.connect(Instrument.colour)
-
-        extractAction2 = QAction('Colour', self)
-        extractAction2.triggered.connect(Instrument.colour)
-
-        impMenu1 = QMenu('Size', self)
+        impMenu1 = QMenu('size', self)
         extractAction3 = QAction('Big', self)
-        extractAction3.triggered.connect(Instrument.size)
-        extractAction4 = QAction('Medium', self)
-        extractAction4.triggered.connect(Instrument.size)
-        extractAction5 = QAction('Small', self)
-        extractAction5.triggered.connect(Instrument.size)
+        extractAction3.triggered.connect(Canvas.big_size)
+        extractAction8 = QAction('Medium', self)
+        extractAction8.triggered.connect(Canvas.medium_size)
+        extractAction9 = QAction('Small', self)
+        extractAction9.triggered.connect(Canvas.small_size)
         impMenu1.addAction(extractAction3)
-        impMenu1.addAction(extractAction4)
-        impMenu1.addAction(extractAction5)
+        impMenu1.addAction(extractAction8)
+        impMenu1.addAction(extractAction9)
 
-        extractAction6 = QAction('Erase', self)
-        extractAction6.triggered.connect(Instrument.colour)
+        extractAction1 = QAction('draw', self)
+        extractAction1.triggered.connect(Canvas.draw)
 
-        extractAction7 = QAction('Save', self)
-        extractAction7.setShortcut('Ctrl+S')
-        extractAction7.triggered.connect(self.saveto)
+        extractAction2 = QAction('colour', self)
+        extractAction2.triggered.connect(Canvas.colour)
 
-        extractAction8 = QAction('Open', self)
-        extractAction8.setShortcut('Ctrl+O')
-        extractAction8.triggered.connect(self.loadfrom)
+        extractAction4 = QAction('erase', self)
+        extractAction4.triggered.connect(Canvas.erase)
 
+        extractAction5 = QAction('save', self)
+        extractAction5.setShortcut('Ctrl+S')
+        extractAction5.triggered.connect(self.saveto)
 
+        extractAction6 = QAction('load', self)
+        extractAction6.setShortcut('Ctrl+L')
+        extractAction6.triggered.connect(self.loadfrom)
         mainMenu = self.menuBar()
 
         fileMenu1 = mainMenu.addMenu('&File')
-        fileMenu1.addAction(extractAction7)
-        fileMenu1.addAction(extractAction8)
+        fileMenu1.addAction(extractAction5)
+        fileMenu1.addAction(extractAction6)
         
         fileMenu2 = mainMenu.addMenu('&Pen')
         fileMenu2.addAction(extractAction1)
@@ -99,35 +175,19 @@ class Window(QMainWindow):
         fileMenu2.addMenu(impMenu1)
         
         fileMenu3 = mainMenu.addMenu('&Eraser')
-        fileMenu3.addAction(extractAction6)
+        fileMenu3.addAction(extractAction4)
         
         self.setGeometry(300, 300, 600, 600)
         self.center()
         self.setWindowTitle('MyPaint')
         self.setWindowIcon(QIcon('Microsoft-Paint-icon.png'))
 
-    @classmethod
-    def saveto(self):
-        name = QFileDialog.getSaveFileName(self, 'Choose file')[0]
-        desktop = QImage(QDesktopWidget())
-        desktop.save(self,format='jpeg')
-
-    @classmethod
-    def loadfrom(self):
-        name = QFileDialog.getOpenFileName(self, 'Choose file')[0]
-        pixmap = QPixmap(name)
-        self.label = QLabel(self)
-        self.label.show()
-        self.label.setPixmap(pixmap)
-        self.label.resize(pixmap.width(), pixmap.height())
-        self.resize(pixmap.width(), pixmap.height())
-
     def closeEvent(self, event):
-        reply = QMessageBox.question(self, 'Потверждение', 'Сохранить файл?', \
+        reply = QMessageBox.question(self, 'Подтверждение', 'Сохранить файл?', \
                                      QMessageBox.Yes | QMessageBox.No, \
                                      QMessageBox.Yes)
         if reply == QMessageBox.Yes:
-            self.saveto()
+            self.saveto
             event.accept()
         else:
             event.accept()
@@ -139,10 +199,23 @@ class Window(QMainWindow):
         qr.moveCenter(cp)
         self.move(qr.topLeft())
 
+    def saveto(self):
+        name = QFileDialog.getSaveFileName(self, 'Choose file')[0]
+
+    def loadfrom(self):
+        name = QFileDialog.getOpenFileName(self, 'Choose file')[0]
+        pixmap = QPixmap(name)
+        self.label = QLabel(self)
+        self.label.show()
+        self.label.setPixmap(pixmap)
+        self.label.resize(pixmap.width(), pixmap.height())
+        self.resize(pixmap.width(), pixmap.height())
+
+
 
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
     ex = Window()
-    ex.show()
+    ex.showMaximized()
     sys.exit(app.exec_())
